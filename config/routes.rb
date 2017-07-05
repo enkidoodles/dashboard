f4efe93af2eb3d09e8683ba2dfd76c4337e6b0c8
Jennie Ron S. Ablog <jennieablog@gmail.com>
2017-07-05 10:14:01 +0800

    Reinitialize

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,3 @@
 Rails.application.routes.draw do
-
-  resources :teams, path_names: {
-    show: ''
-  }
-
-  resources :teams, except: [:index]
-  resources :branches do
-    resources :teams
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20170704083110_add_branch_id_to_teams.rb
+++
@@ -1,5 +0,0 @@
-class AddBranchIdToTeams < ActiveRecord::Migration[5.0]
-  def change
-    add_column :teams, :branch_id, :integer
-  end
-end
